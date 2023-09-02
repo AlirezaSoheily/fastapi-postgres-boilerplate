c1a0045fc139aec79a87afcd798111b513287c6a
Dark Ghost <alirezasoheili20@gmail.com>
2023-09-03 03:02:11 +0330

    move models from library to user

--- app/app/models/user.py
+++ app/app/models/user.py
@@ -1,6 +1,8 @@
 from sqlalchemy import Table, Boolean, ForeignKey, Column, Integer, String
 from sqlalchemy.orm import relationship
 from ..db.base_class import Base
+from sqlalchemy.sql.sqltypes import DateTime
+from datetime import datetime
 
 
 class User(Base):
@@ -22,6 +24,49 @@ class Client(Base):
     is_restricted = Column(Boolean(), default=False)
     buy = relationship('Buy', back_populates='client')
     borrow = relationship('Borrow', back_populates='client')
+
+
+class Category(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    daily_borrow_fee = Column(Integer)
+    max_borrow_amount = Column(Integer)
+    books = relationship('Book', back_populates='category')
+
+
+class Book(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    author = Column(String, index=True)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship('Category', back_populates='book')
+    stock_amount = Column(Integer)
+    salable_quantity = Column(Integer)
+    price = Column(Integer)
+    buy = relationship('Buy', back_populates='book')
+    borrow = relationship('Borrow', back_populates='book')
+
+
+class Buy(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    bought_date = Column(DateTime(timezone=True), default=datetime.utcnow, index=True)
+    book_id = Column(Integer, ForeignKey('book.id'))
+    book = relationship('Book', back_populates='buy')
+    client_id = Column(Integer, ForeignKey('client.id'))
+    client = relationship('Client', back_populates='buy')
+
+
+class Borrow(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    borrowed_date = Column(DateTime(timezone=True), default=datetime.utcnow, index=True)
+    borrow_days = Column(Integer, default=0)
+    returned_date = Column(DateTime(timezone=True), default=None, index=True, nullable=True)
+    book_id = Column(Integer, ForeignKey('book.id'))
+    book = relationship('Book', back_populates='borrow')
+    client_id = Column(Integer, ForeignKey('client.id'))
+    client = relationship('Client', back_populates='borrow')
+
+
 
 # Tables for many-to-many relation of user-buy and user-borrow
 # user_buy_association = Table(
