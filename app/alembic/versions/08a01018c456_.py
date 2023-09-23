d78a3eb18b476d601483754efa461cb2821099b5
Dark Ghost <alirezasoheili20@gmail.com>
2023-09-23 20:29:46 +0330

    Add: add migration files

---
+++ app/alembic/versions/08a01018c456_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 08a01018c456
+Revises: 8527224a4245
+Create Date: 2023-09-07 17:37:37.991950
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '08a01018c456'
+down_revision = '8527224a4245'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ app/alembic/versions/8527224a4245_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 8527224a4245
+Revises: 89d466bc833b
+Create Date: 2023-09-07 17:18:59.818259
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8527224a4245'
+down_revision = '89d466bc833b'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
